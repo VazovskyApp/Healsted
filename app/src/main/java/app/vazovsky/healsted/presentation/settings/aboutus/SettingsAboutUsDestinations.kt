bf4c37794166d99ea3791fef2ab14f6c903a2167
vazovsky <46650184+vazovsky17@users.noreply.github.com>
2023-01-04 15:48:57 +0400

    add const strings

--- app/src/main/java/app/vazovsky/healsted/presentation/settings/aboutus/SettingsAboutUsDestinations.kt
+++ app/src/main/java/app/vazovsky/healsted/presentation/settings/aboutus/SettingsAboutUsDestinations.kt
@@ -1,22 +1,23 @@
 package app.vazovsky.healsted.presentation.settings.aboutus
 
+import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import app.vazovsky.healsted.R
 import app.vazovsky.healsted.presentation.base.NavigationCommand
+import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 
-private const val TELEGRAM_CHANNEL_URL = "tg://resolve?domain=healsted"
-private const val TELEGRAM_VAZOVSKY_URL = "tg://resolve?domain=vazovsky_app"
-private const val SHARE_URL = "https://www.t.me/healsted"
 private const val DEFAULT_SHARE_TYPE = "text/plain"
 
-
-class SettingsAboutUsDestinations @Inject constructor() {
+class SettingsAboutUsDestinations @Inject constructor(
+    @ApplicationContext val context: Context,
+) {
     fun shareUrl() = NavigationCommand.Activity(
         Intent.createChooser(
             Intent().apply {
                 action = Intent.ACTION_SEND
-                putExtra(Intent.EXTRA_TEXT, SHARE_URL)
+                putExtra(Intent.EXTRA_TEXT, context.getString(R.string.share_link))
                 type = DEFAULT_SHARE_TYPE
             }, null
         )
@@ -25,14 +26,14 @@ class SettingsAboutUsDestinations @Inject constructor() {
     fun telegramChannel() = NavigationCommand.Activity(
         Intent().apply {
             action = Intent.ACTION_VIEW
-            data = Uri.parse(TELEGRAM_CHANNEL_URL)
+            data = Uri.parse(context.getString(R.string.telegram_channel_link))
         }
     )
 
     fun telegramVazovsky() = NavigationCommand.Activity(
         Intent().apply {
             action = Intent.ACTION_VIEW
-            data = Uri.parse(TELEGRAM_VAZOVSKY_URL)
+            data = Uri.parse(context.getString(R.string.telegram_vazovsky_link))
         }
     )
 }
